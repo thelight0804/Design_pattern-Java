3adbffb2714850b24914b5331ba9e54f06205c57
Mina-1316 <mina1316.canreply@gmail.com>
2022-06-08 09:07:11 +0900

    feat: add missing sales repository class
    가계 정보를 담당하는 SalesRepository 클래스 완료.
    TODO: 차후 Database에 연결하시기 바랍니다.

---
+++ source/src/main/java/repository/SalesRepository.java
@@ -0,0 +1,37 @@
+package repository;
+
+import sales.Sales;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is used to store the sales information.
+ * Can be connected to the database.
+ * @version 1.0
+ */
+public class SalesRepository {
+/*
+TODO: you should connect this class to the database On production
+*/
+    List<Sales> salesStorage = new ArrayList<>();
+
+    // Uses signleton pattern
+    private static SalesRepository instance = null;
+
+    public static SalesRepository getInstance() {
+        if (instance == null) {
+            instance = new SalesRepository();
+        }
+        return instance;
+    }
+
+    public void addSales(Sales sales) {
+        salesStorage.add(sales);
+    }
+
+    public List<Sales> getSales() {
+        return salesStorage;
+    }
+
+}
