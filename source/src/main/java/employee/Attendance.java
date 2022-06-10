594df12715931e6ff9662d1bcfe482afe3eb830f
soochangoforit <tncksdl05@gmail.com>
2022-06-10 18:36:39 +0900

    feat: add attendance entity and repository
    attendance entity is work as a work log schedule

---
+++ source/src/main/java/employee/Attendance.java
@@ -0,0 +1,18 @@
+package employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@AllArgsConstructor @Builder
+public class Attendance {
+    private Employee employee;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Long wage;
+
+}
