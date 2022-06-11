33e40f593edaa614d995a7d01d297834e8b86ae7
Mina-1316 <mina1316.canreply@gmail.com>
2022-06-11 15:38:39 +0900

    refactor: optimize operation selection to enum
    옵션 설정을 enum에 기반한 방식으로 변경

--- source/src/main/java/menu/MenuManagement.java
+++ source/src/main/java/menu/MenuManagement.java
@@ -1,10 +1,10 @@
 package menu;
 
-import com.sun.xml.internal.ws.util.UtilException;
+import conf.enums.UserType;
 import conf.interfaces.EndpointElement;
 import conf.interfaces.Manager;
+import employee.Employee;
 import menu.interfaces.NotifyElement;
-import menu.interfaces.Observer;
 import menu.observers.AppNotificationSender;
 import menu.observers.OutdoorBoardModifierModule;
 import menu.observers.SMSNotificationSender;
@@ -14,13 +14,14 @@ import repository.exception.EntityNotFoundException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.function.Function;
 
 public class MenuManagement extends NotifyElement implements Manager {
     private final MenuRepository menuRepository;
 
     // Use singleton pattern
     private static volatile MenuManagement instance = null;
+
     public static MenuManagement getInstance() {
         if (instance == null) {
             synchronized (MenuManagement.class) {
@@ -38,75 +39,67 @@ public class MenuManagement extends NotifyElement implements Manager {
     }
 
     public void createMenu() {
-        try{
-        // Create menu
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Name: ");
-        String name = br.readLine();
-        System.out.print("Price: ");
-        int price = Integer.parseInt(br.readLine());
-        this.menuRepository.createMenu(Menu.builder()
-                .name(name)
-                .price(price).build());
-        this.onCreate(name, price);
-        }
-        catch (IOException e) {
+        try {
+            // Create menu
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Name: ");
+            String name = br.readLine();
+            System.out.print("Price: ");
+            int price = Integer.parseInt(br.readLine());
+            this.menuRepository.createMenu(Menu.builder()
+                    .name(name)
+                    .price(price).build());
+            this.onCreate(name, price);
+        } catch (IOException e) {
             System.err.println("입력 중 문제가 발생했습니다.");
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.err.println("잘못된 값을 입력했습니다.");
         }
     }
 
     public void modifyMenu() {
-        try{
+        try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Menu: ");
+            System.out.print("수장할 메뉴 이름을 입력하세요: ");
             String prevName = br.readLine();
-            System.out.print("Name to change: ");
+            System.out.print("변경할 메뉴 이름을 입력하세요(변경사항 없을 시 그대로 입력하시기 바랍니다.): ");
             String name = br.readLine();
-            System.out.print("Price to change: ");
+            System.out.print("변경된 메뉴 가격을 입력하세요: ");
             int price = Integer.parseInt(br.readLine());
             Menu menu = this.menuRepository.getMenuByName(prevName).orElseThrow(EntityNotFoundException::new);
             int prevPrice = menu.getPrice();
             menu.setName(name);
             menu.setPrice(price);
             this.onModify(prevName, prevPrice, name, price);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.err.println("입력 중 문제가 발생했습니다.");
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.err.println("잘못된 값을 입력했습니다.");
-        }
-        catch (EntityNotFoundException e) {
+        } catch (EntityNotFoundException e) {
             System.err.println("존재하지 않는 메뉴입니다.");
         }
     }
 
     public void deleteMenu() {
-        try{
+        try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("Name: ");
             String name = br.readLine();
             Menu menu = this.menuRepository.getMenuByName(name).orElseThrow(EntityNotFoundException::new);
             this.menuRepository.deleteMenu(menu);
             this.onDelete(menu.getName());
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.err.println("입력 중 문제가 발생했습니다.");
-        }
-        catch (EntityNotFoundException e){
+        } catch (EntityNotFoundException e) {
             System.out.println("메뉴 데이터가 존재하지 않습니다.");
         }
     }
 
     @Override
     public void run() {
-        while(true) {
+        while (true) {
             if (!Manager.invokeMenu(MenuManagementEndpoint.values())) break;
         }
-
     }
 
     // Run this method to do example runs
@@ -121,44 +114,57 @@ public class MenuManagement extends NotifyElement implements Manager {
         AppNotificationSender appNotificationSender = new AppNotificationSender();
         OutdoorBoardModifierModule outdoorBoardModifierModule = new OutdoorBoardModifierModule();
         // Add observer
-        menuManagement.addObserver(smsNotificationSender);
-        menuManagement.addObserver(appNotificationSender);
-        menuManagement.addObserver(outdoorBoardModifierModule);
+        menuManagement.addObserver("SMS", smsNotificationSender);
+        menuManagement.addObserver("APP", appNotificationSender);
+        menuManagement.addObserver("OutdoorBoard", outdoorBoardModifierModule);
         // Run service
         menuManagement.run();
     }
 
     private enum MenuManagementEndpoint implements EndpointElement {
-        CREATE_MENU{
-            @Override public String getName() {
+        CREATE_MENU {
+            @Override
+            public String getName() {
                 return "메뉴 생성";
             }
-            @Override public String getDescription() {
+            @Override
+            public String getDescription() {
                 return "메뉴를 생성합니다.";
             }
-            @Override public Runnable getRunner()  {
+            @Override
+            public Function<UserType, Boolean> requireAuthentication() {
+                return userType -> userType == UserType.ADMIN;
+            }
+            @Override
+            public Runnable getRunner() {
                 return MenuManagement.getInstance()::createMenu;
             }
         },
-        MODIFY_MENU{
+        MODIFY_MENU {
             @Override public String getName() {
                 return "메뉴 수정";
             }
             @Override public String getDescription() {
                 return "메뉴를 수정합니다.";
             }
-            @Override public Runnable getRunner()  {
+            @Override public Function<UserType, Boolean> requireAuthentication() {
+                return userType -> userType == UserType.ADMIN;
+            }
+            @Override public Runnable getRunner() {
                 return MenuManagement.getInstance()::modifyMenu;
             }
         },
-        DELETE_MENU{
+        DELETE_MENU {
             @Override public String getName() {
                 return "메뉴 삭제";
             }
             @Override public String getDescription() {
                 return "메뉴를 삭제합니다.";
             }
-            @Override public Runnable getRunner()  {
+            @Override public Function<UserType, Boolean> requireAuthentication() {
+                return userType -> userType == UserType.ADMIN;
+            }
+            @Override public Runnable getRunner() {
                 return MenuManagement.getInstance()::deleteMenu;
             }
         },
